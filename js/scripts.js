af17a8100ca19bb30c4e58a677be974b33e3cdb0
Maro <grafika.marekduda@gmail.com>
2017-06-04 23:11:23 +0200

    deleted nameUpperCased

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.'
-var dinosaur = 'triceratops'
-
 var name = 'triceratops';
-var nameUpperCased = name.toUpperCase();
 
 var textCharsAfter = text.replace('Velociraptor', name.toUpperCase());
 
-var partOfTextCharsAfter = textCharsAfter.slice(0,72);
+var partOfTextCharsAfter = textCharsAfter.slice(0, 72);
 
 console.log(partOfTextCharsAfter);
